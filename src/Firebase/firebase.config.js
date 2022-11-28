e832c4732f733be4ed9d5a13b9056c5509b816c3
unknown <nayeemhaque43@gmail.com>
2022-11-28 21:15:42 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ0kqGig9vkE5686_nD7PrIxxZF255mcg",
+  authDomain: "mobile-paradise-7b27d.firebaseapp.com",
+  projectId: "mobile-paradise-7b27d",
+  storageBucket: "mobile-paradise-7b27d.appspot.com",
+  messagingSenderId: "26378878674",
+  appId: "1:26378878674:web:dc8b26c7f7705687c9770b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="hero bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Mobile Paradise</h1>
+          <p className="py-6">
+            Best phone resale-commerce in Bangladesh. Sell your
+            Products Now!
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
